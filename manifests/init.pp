095327892e581e747397eee190bed2b1385e657a
Martin Gondermann <magicmonty@users.noreply.github.com>
2015-02-28 14:37:44 +0100

    Updated URL

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class handbrake($version = '0.10.0') {
   package { 'HandBrake':
     provider => 'appdmg',
-    source   => "http://handbrake.fr/rotation.php?file=HandBrake-${version}-MacOSX.6_GUI_x86_64.dmg",
+    source   => "http://download.handbrake.fr/handbrake/releases/${version}/HandBrake-${version}-MacOSX.6_GUI_x86_64.dmg",
   }
 }
