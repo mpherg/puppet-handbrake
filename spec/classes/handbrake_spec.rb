f8451024829a14fcdb63f16ab7939dea5cd2a7fc
Martin Gondermann <magicmonty@users.noreply.github.com>
2015-02-28 14:30:52 +0100

    Update handbrake_spec.rb

--- spec/classes/handbrake_spec.rb
+++ spec/classes/handbrake_spec.rb
@@ -4,7 +4,7 @@ describe 'handbrake' do
   it do
     should contain_package('HandBrake').with({
       :provider => 'appdmg',
-      :source   => 'http://sourceforge.net/projects/handbrake/files/0.9.9/HandBrake-0.9.9-MacOSX.6_GUI_x86_64.dmg',
+      :source   => 'http://handbrake.fr/rotation.php?file=HandBrake-0.10.0-MacOSX.6_GUI_x86_64.dmg',
     })
   end
 end
